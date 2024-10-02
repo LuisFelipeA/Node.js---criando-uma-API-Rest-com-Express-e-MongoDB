1e70e0ee89c63609e6ca97cd14a1e8a64bb04523
LuisFelipeAgit config --global user.name LuisFelipeAgit config --global user.name LuisFelipeA <felipeadriani2@gmail.com>
2024-10-02 19:41:22 -0300

    CRUD Livros (Sem persistir no banco)

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from 'http';
+import app from "./src/app.js"
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js 2",
-    "/rota1": "Rota 1",
-    "/rota2": "Rota 2"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-Type": "text/plain"});
-    res.end(rotas[req.url]);
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor rodando")
 } )
